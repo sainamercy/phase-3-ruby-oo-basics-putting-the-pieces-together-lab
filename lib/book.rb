76f0a5f69287ec24aa37510525b21f823f318896
sainamercy <sainamercy@gmail.com>
2023-02-10 11:41:13 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
-
-
+class Book
+    attr_accessor :title, :author, :page_count, :turn_page, :genre
+    def initialize(title)
+        @title = title    
+    end
+    def author=(author)
+        @author = author
+    end
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+    def genre=(genre)
+        @genre = genre
+    end
+    def turn_page
+        puts 'Flipping the page...wow, you read fast!'
+    end
+end
+book = Book.new("Ruby")
+puts book.title
+book.author = "Rubist"
+puts book.author
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand)
+        @brand = brand
+    end
+    def color=(color)
+        @color = color
+    end
+    def size=(size)
+        @size = size
+    end
+    def material=(material)
+        @material = material
+    end
+    def condition=(condition)
+        @condition = condition
+    end
+    def cobble
+        puts 'Your shoe is as good as new!'
+        @condition = "new"
+    end
+end
+shoe = Shoe.new("Nike")
+puts shoe.brand
+shoe.color = "red"
+puts shoe.color
+shoe.material= "leather" 
+puts shoe.material 
+shoe.cobble
+puts 
